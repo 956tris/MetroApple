9079ad8226270712ed06f374295c39604b9d4b5b
956tris <956tris@users.noreply.github.com>
2026-07-28 01:07:20 +0100

    bump ver to 6.0.2-hotfix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ if (localPropertiesFile.exists()) {
 }
 
 val baseApplicationId = "com.metrofuse.music"
-val metroFuseVersionCode = 600
-val metroFuseVersionName = "6.0"
+val metroFuseVersionCode = 602
+val metroFuseVersionName = "6.0.2"
 val metroFuseUpdateRepository = "956tris/MetroFuse"
 val discordRpcApplicationId = "1508739806186963045"
 val applicationIdOverride = System.getenv("METROLIST_APPLICATION_ID")?.takeIf { it.isNotBlank() }
